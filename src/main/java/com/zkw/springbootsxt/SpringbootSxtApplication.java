49077e9ac9cc224710f4fd43bc18ea0f209accc2
zhkw <13910066913@163.com>
2020-10-01 11:51:18 +0800

    init

---
+++ src/main/java/com/zkw/springbootsxt/SpringbootSxtApplication.java
@@ -0,0 +1,13 @@
+package com.zkw.springbootsxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSxtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkw/springbootsxt/SpringbootSxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkw.springbootsxt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSxtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
